ea56136b243b17a92f1ced358153f6b91f623402
rsmnarts <rismanalkarim@gmail.com>
2020-12-12 17:21:54 +0700

    fix code

--- app/src/main/java/com/example/speectotextandwprdcloud/MainActivity.java
+++ app/src/main/java/com/example/speectotextandwprdcloud/MainActivity.java
@@ -1,11 +1,7 @@
 package com.example.speectotextandwprdcloud;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -22,16 +18,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editText;
-    ImageView imageView;
+    public static final Integer RecordAudioRequestCode = 1;
 
-    public static final Integer RecordAudioRequestCode=1;
     private SpeechRecognizer speechRecognizer;
 
+    EditText editText;
+    ImageView imageView;
     AlertDialog.Builder alertSpeechDialog;
     AlertDialog alertDialog;
 
@@ -40,103 +41,106 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText=findViewById(R.id.EditText);
-        imageView=findViewById(R.id.imageView);
+        editText = findViewById(R.id.EditText);
+        imageView = findViewById(R.id.imageView);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)!=
-                PackageManager. PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             checkPermission();
-
-            speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
-
-            final Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-
-            speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-
-            speechRecognizer.setRecognitionListener(new RecognitionListener() {
-                @Override
-                public void onReadyForSpeech(Bundle params) {
-
-                }
-
-                @Override
-                public void onBeginningOfSpeech() {
-                    ViewGroup viewGroup = findViewById(android.R.id.content);
-                    View dialogView = LayoutInflater.from(MainActivity.this).inflate(R.layout.alertcustom,
-                            viewGroup, false);
-
-                    alertSpeechDialog = new AlertDialog.Builder(MainActivity.this);
-                    alertSpeechDialog.setMessage("Listening...");
-                    alertSpeechDialog.setView(dialogView);
-                    alertDialog = alertSpeechDialog.create();
-                    alertDialog.show();
-
-                }
-
-                @Override
-                public void onRmsChanged(float rmsdB) {
-
-                }
-
-                @Override
-                public void onBufferReceived(byte[] buffer) {
-
-                }
-
-                @Override
-                public void onEndOfSpeech() {
-
-                }
-
-                @Override
-                public void onError(int error) {
-
-                }
-
-                @Override
-                public void onResults(Bundle bundle) {
-                    imageView.setImageResource(R.drawable.ic_baseline_keyboard_voice_24);
-                    ArrayList<String> arrayList = bundle.getStringArrayList(SpeechRecognizer. RESULTS_RECOGNITION);
-                    editText.setText(arrayList.get(0));
-                    alertDialog.dismiss();
-
-                }
-
-                @Override
-                public void onPartialResults(Bundle partialResults) {
-
-                }
-
-                @Override
-                public void onEvent(int eventType, Bundle params) {
-
-                }
-            });
-            imageView.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent motionEvent) {
-                    if(motionEvent.getAction()==MotionEvent.ACTION_UP){
-                        speechRecognizer.stopListening();
-                    }
-                    if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
-                        imageView.setImageResource(R.drawable.ic_baseline_keyboard_voice_24);
-                        speechRecognizer.startListening(speechIntent);
-                    }
-                    return false;
-                }
-            });
         }
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        final Intent speechIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        speechIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(MainActivity.this)
+                        .inflate(R.layout.alertcustom, viewGroup, false);
+
+                alertSpeechDialog = new AlertDialog.Builder(MainActivity.this);
+                alertSpeechDialog.setMessage("Listening...");
+                alertSpeechDialog.setView(dialogView);
+                alertDialog = alertSpeechDialog.create();
+                alertDialog.show();
+
+            }
+
+            @Override
+            public void onRmsChanged(float rmsdB) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int error) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                imageView.setImageResource(R.drawable.ic_baseline_keyboard_voice_24);
+                ArrayList<String> arrayList = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+                editText.setText(arrayList.get(0));
+                alertDialog.dismiss();
+            }
+
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+
+            }
+
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    speechRecognizer.stopListening();
+                }
+
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    imageView.setImageResource(R.drawable.ic_baseline_keyboard_voice_24);
+                    speechRecognizer.startListening(speechIntent);
+                }
+
+                return false;
+            }
+        });
     }
 
+
     private void checkPermission() {
-        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{
-                    Manifest.permission.RECORD_AUDIO}, RecordAudioRequestCode);
-
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.RECORD_AUDIO
+            }, RecordAudioRequestCode);
         }
-
     }
 
     @Override
@@ -149,10 +153,10 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode==RecordAudioRequestCode && grantResults.length>0){
-
-            Toast.makeText(this, "Permission Granted",Toast.LENGTH_LONG).show();
-
+        if (requestCode == RecordAudioRequestCode && grantResults.length > 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
